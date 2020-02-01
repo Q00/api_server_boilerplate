dae69254a72a23b4eb797817f155608fb74670f5
Q00 <jqyu.lee@gmail.com>
2020-02-01 18:00:44 +0900

    feat: ⚡️ make server.ts

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import express, { Request, NextFunction, Response } from 'express';
+import { Container } from 'typedi';
+import './utils/env';
+import { useContainer, useExpressServer } from 'routing-controllers';
+import { routingControllerOptions } from './utils/routingConfig';
+
+useContainer(Container);
+const app = express();
+console.log(`Current NODE_ENV is ${process.env.NODE_ENV}`);
+
+useExpressServer(app, routingControllerOptions);
+export function runServer(host: string, port: number) {
+  return new Promise((resolve, reject) => {
+    app.listen(port, host, (err: any) => {
+      if (err) {
+        reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+app.use((err: string, _req: Request, res: Response, _next: NextFunction) => {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.end(`{ result: false, error: ${err} }`);
+});
+
+export { app };
