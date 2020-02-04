cac962e61ea0a497668eeae2ad3debb866fce4c6
Q00 <jqyu.lee@gmail.com>
2020-02-04 17:34:46 +0900

    feat: ⚡️ database connection config

---
+++ src/database/config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import DatabaseNamingStrategy from './NamingStrategy';
+import '../utils/env';
+const typeOrmConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  namingStrategy: new DatabaseNamingStrategy(),
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DATABASE,
+  synchronize: false,
+  logging: false,
+  entities: [`${path.join(__dirname, '..', 'models')}/**.[tj]s`],
+  migrations: [`${path.join(__dirname, '..', 'models')}/migration/**.[tj]s`],
+};
+
+export { typeOrmConfig };
