a12f566b631ffe5e108de159e6c7b84f60150a75
Q00 <jqyu.lee@gmail.com>
2020-02-05 23:39:38 +0900

    feat: ⚡️ make comment model

---
+++ src/model/BaseComment.ts
@@ -0,0 +1,16 @@
+import { Column } from 'typeorm';
+import { BaseModel } from './BaseModel';
+import { IsString, MaxLength, IsInt } from 'class-validator';
+
+// you can extends this class making child comment and add user
+
+export abstract class BaseComment extends BaseModel {
+  @Column({ length: 50 })
+  @IsString()
+  @MaxLength(50)
+  public comment!: string;
+
+  @Column({ default: 0 })
+  @IsInt()
+  public reportCount!: number;
+}
