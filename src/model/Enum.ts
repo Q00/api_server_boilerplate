84fb6e1d04c4bd9395e42e561d6d3eed360aa19a
Q00 <jqyu.lee@gmail.com>
2020-02-05 23:41:50 +0900

    feat: ⚡️ make enum  file

---
+++ src/model/Enum.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+// you can put your enum here
+
+export enum Provider {
+  GOOGLE = 'google',
+  KAKAO = 'kakao',
+  NAVER = 'naver',
+  LOCAL = 'local',
+}
+
+export enum Method {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  DELETE = 'delete',
+}
