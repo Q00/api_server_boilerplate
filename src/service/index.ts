49cfcfcd6573b719861bb5b223e604dad9e04d2b
Q00 <jqyu.lee@gmail.com>
2020-02-06 00:36:06 +0900

    feat: ⚡️ make service index file

---
+++ src/service/index.ts
@@ -0,0 +1,7 @@
+export { UserService } from './UserService';
+export { UserAccountService } from './UserAccountService';
+export { BaseBoardService } from './BaseBoardService';
+export { ExampleBoardService } from './ExampleBoardService';
+export { ExampleBoardCommentService } from './ExampleBoardCommentService';
+export { ExampleBoardDepthCommentService } from './ExampleBoardDepthCommentService';
+export { ApiLogService } from './ApiLogService';
