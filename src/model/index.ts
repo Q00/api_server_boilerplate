a444fe58e2bbc3db6778e9e82495a1ec728663f9
Q00 <jqyu.lee@gmail.com>
2020-02-05 23:42:39 +0900

    feat: ⚡️ make model index file

---
+++ src/model/index.ts
@@ -0,0 +1,7 @@
+// export module
+export { User } from './Users';
+export { UserAccount } from './UserAccounts';
+export { ExampleBoard } from './ExampleBoards';
+export { ExampleBoardComment } from './ExampleBoardComments';
+export { ExampleBoardDepthComment } from './ExampleBoardDepthComments';
+export { APILog } from './APIlogs';
